d82f1c9b72c93cb356f5943822d0ea8941e02e8e
Julien Marbach <julien.marbach@dental-wings.com>
2020-10-26 01:28:03 -0400

    progress on layout wrapper
    corne, idobo and ergodash done
    quefrency todo

--- keyboards/crkbd/keymaps/jumar/keymap.c
+++ keyboards/crkbd/keymaps/jumar/keymap.c
@@ -8,58 +8,48 @@ extern rgblight_config_t rgblight_config;
 
 extern uint8_t is_master;
 
-enum layer_names {
-  _0_QWERTY = 0,
-  _1_SYMBOLS_NUMPAD,
-  _2_MOUSE_MEDIA,
-  _3_NAV,
-  _4_FN,
-  _5_RGB,
-  _6_GAMING,
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_0_QWERTY] = LAYOUT_wrapper( \
-        ROW4_LEFT_BASE      ,                   ROW4_RGHT_BASE      ,
-        ROW3_LEFT_BASE      ,                   ROW3_RGHT_BASE      ,
-        ROW2_LEFT_BASE      ,                   ROW2_RGHT_BASE      ,
-               KC_LGUI, ALT_BSP, LT3_SPC, LT3_ENT, LT1_DEL , LT4_GRV
+	[_0_QWERTY] = LAYOUT_wrapper(
+        ROW4_LEFT_BASE, ROW4_RIGHT_BASE,
+        ROW3_LEFT_BASE, ROW3_RIGHT_BASE,
+        ROW2_LEFT_BASE, ROW2_RIGHT_BASE,
+        ROW1_LEFT_BASE, ROW1_RIGHT_BASE
     ),
-	[_1_SYMBOLS_NUMPAD] = LAYOUT(
-        KC_ESC , KC_EXLM, KC_EQL , KC_LPRN, KC_RPRN, KC_PIPE,                   KC_UNDS, KC_P7  , KC_P8  , KC_P9  , KC_PPLS, _______,
-        _______, KC_PERC, KC_ASTR, KC_LCBR, KC_RCBR, KC_AMPR,                   KC_AT  , KC_P4  , KC_P5  , KC_P6  , KC_PMNS, KC_NLCK,
-        _______, KC_HASH, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                   KC_DLR,  KC_P1  , KC_P2  , KC_P3  , KC_PAST, KC_PENT,
-                                            KC_APP , TG(_6_GAMING), _______, _______, _______, KC_P0
+	[_1_SYMBOLS_NUMPAD] = LAYOUT_wrapper(
+        ROW4_LEFT_SYMB_NUMPAD, ROW4_RIGHT_SYMB_NUMPAD,
+        ROW3_LEFT_SYMB_NUMPAD, ROW3_RIGHT_SYMB_NUMPAD,
+        ROW2_LEFT_SYMB_NUMPAD, ROW2_RIGHT_SYMB_NUMPAD,
+        ROW1_LEFT_SYMB_NUMPAD, ROW1_RIGHT_SYMB_NUMPAD
     ),
-	[_2_MOUSE_MEDIA] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_WH_U, XXXXXXX, KC_MS_U, XXXXXXX, KC_WH_U, _______,
-        XXXXXXX, _______, _______, XXXXXXX, _______, XXXXXXX,                   KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
-                                            XXXXXXX, KC_BTN3, KC_BTN2, KC_BTN1, KC_MPLY, KC_MFFD
+	[_2_MOUSE_MEDIA] = LAYOUT_wrapper(
+        ROW4_LEFT_MOUSE_MEDIA, ROW4_RIGHT_MOUSE_MEDIA,
+        ROW3_LEFT_MOUSE_MEDIA, ROW3_RIGHT_MOUSE_MEDIA,
+        ROW2_LEFT_MOUSE_MEDIA, ROW2_RIGHT_MOUSE_MEDIA,
+        ROW1_LEFT_MOUSE_MEDIA, ROW1_RIGHT_MOUSE_MEDIA
     ),
-	[_3_NAV] = LAYOUT(
-        KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_PGUP, KC_HOME, KC_UP  , KC_END,  XXXXXXX, KC_PSCR,
-        XXXXXXX, XXXXXXX, _______, _______, _______, XXXXXXX,                   KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                            _______, _______, _______, _______, _______, XXXXXXX
+	[_3_NAV] = LAYOUT_wrapper(
+        ROW4_LEFT_NAV_CPY_PASTA, ROW4_RIGHT_NAV_CPY_PASTA,
+        ROW3_LEFT_NAV_CPY_PASTA, ROW3_RIGHT_NAV_CPY_PASTA,
+        ROW2_LEFT_NAV_CPY_PASTA, ROW2_RIGHT_NAV_CPY_PASTA,
+        ROW1_LEFT_NAV_CPY_PASTA, ROW1_RIGHT_NAV_CPY_PASTA
     ),
-	[_4_FN] = LAYOUT(
-        KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                   KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
-        KC_LSFT, XXXXXXX, _______, XXXXXXX, _______, XXXXXXX,                   XXXXXXX, _______, KC_PGUP, KC_HOME, KC_UP  , KC_END ,
-        KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,
-                                            _______, _______, _______, _______, _______, _______
+    [_4_GAM] = LAYOUT_wrapper(
+        ROW4_LEFT_GAMING, ROW4_RIGHT_GAMING,
+        ROW3_LEFT_GAMING, ROW3_RIGHT_GAMING,
+        ROW2_LEFT_GAMING, ROW2_RIGHT_GAMING,
+        ROW1_LEFT_GAMING, ROW1_RIGHT_GAMING
     ),
-  [_5_RGB] = LAYOUT(
-        RESET  , XXXXXXX, XXXXXXX, KC_GRV , XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-        XXXXXXX, XXXXXXX, RGB_SAI, RGB_HUI, RGB_VAI, RGB_SPI,                   XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, RGB_SAD, RGB_HUD, RGB_VAD, RGB_SPD,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                            XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG, XXXXXXX, XXXXXXX
+   [_5_RGB] = LAYOUT_wrapper(
+        ROW4_LEFT_RGB, ROW4_RIGHT_RGB,
+        ROW3_LEFT_RGB, ROW3_RIGHT_RGB,
+        ROW2_LEFT_RGB, ROW2_RIGHT_RGB,
+        ROW1_LEFT_RGB, ROW1_RIGHT_RGB
     ),
-  [_6_GAMING] = LAYOUT(
-        KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , XXXXXXX,
-        KC_TAB , KC_Q   , LSFT_W , KC_E   , LCTL_R , KC_T   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_UP  , XXXXXXX,
-        XXXXXXX, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                   KC_N   , KC_M   , KC_COMM, KC_LEFT, KC_DOWN, KC_RGHT,
-                                            XXXXXXX, KC_LALT, KC_SPC , KC_ENT , KC_DEL , TG(_6_GAMING)
+   [_6_FN] = LAYOUT_wrapper(
+        ROW4_LEFT_FN, ROW4_RIGHT_FN,
+        ROW3_LEFT_FN, ROW3_RIGHT_FN,
+        ROW2_LEFT_FN, ROW2_RIGHT_FN,
+        ROW1_LEFT_FN, ROW1_RIGHT_FN
     ),
 };
 
@@ -87,6 +77,7 @@ void matrix_init_user(void) {
     #ifdef SSD1306OLED
       iota_gfx_init(!has_usb());   // turns on the display
     #endif
+    matrix_init_user_RGB_LYR();
 }
 
 //SSD1306 OLED update loop, make sure to add #define SSD1306OLED in config.h
@@ -117,6 +108,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #endif
     set_timelog();
   }
+  process_record_user_RGB(keycode, record);
   return true;
 }
 
@@ -139,14 +131,14 @@ const char *read_layer_state(void) {
     case _3_NAV:
       strcat(matrix_line_str, "Navigation");
       break;
-    case _4_FN:
-      strcat(matrix_line_str, "FN/Nav");
+    case _4_GAM:
+      strcat(matrix_line_str, "Gaming");
       break;
     case _5_RGB:
       strcat(matrix_line_str, "RGB");
       break;
-    case _6_GAMING:
-      strcat(matrix_line_str, "Gaming");
+    case _6_FN:
+      strcat(matrix_line_str, "FN/Nav");
       break;
     default:
       sprintf(matrix_line_str + strlen(matrix_line_str), "Unknown (%d)", layer);
--- keyboards/keebio/quefrency/keymaps/jumar/config.h
+++ keyboards/keebio/quefrency/keymaps/jumar/config.h
@@ -3,6 +3,7 @@ This is the c configuration file for the keymap
 
 Copyright 2012 Jun Wako <wakojun@gmail.com>
 Copyright 2015 Jack Humbert
+Copyright 2018 Danny Nguyen <danny@keeb.io>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -20,9 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-//#define USE_MATRIX_I2C
+/* Use I2C or Serial, not both */
 
-#define SSD1306OLED
-
-#define USE_SERIAL_PD2
-
+#define USE_SERIAL
+// #define USE_I2C
