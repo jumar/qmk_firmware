38dffe300f582324cc08501ee5a6ac15da62b28f
Julien Marbach <julien.marbach@dental-wings.com>
2020-12-23 23:29:44 -0500

    update quefrencuy layout

--- keyboards/keebio/quefrency/keymaps/jumar/keymap.c
+++ keyboards/keebio/quefrency/keymaps/jumar/keymap.c
@@ -4,25 +4,25 @@
 
 extern keymap_config_t keymap_config;
   /*
-   * ┌─────┬─────┐┌─────┬─────┬─────┬─────┬─────┬─────┬─────┐     ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐┌─────┐
-   * │ F1  │ F2  ││ESC  │  1  │  2  │  3  │  4  │  5  │  6  │     │  7  │  8  │  9  │  0  │  -  │  =  │BSLS │ DEL ││HOME │
-   * ├─────┼─────┤├─────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┘  ┌──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴─────┤├─────┤
-   * │ F3  │ F4  ││TAB     │  Q  │  W  │  E  │  R  │  T  │     │  Y  │  U  │  I  │  O  │  P  │  [  │  ]  │BSPC    ││END  │
+   * ┌─────┬─────┐┌─────┬─────┬─────┬─────┬─────┬─────┬─────┐     ┌─────┬─────┬─────┬─────┬─────┬─────┬───────────┐┌─────┐
+   * │ F1  │ESC  ││GRV  │  1  │  2  │  3  │  4  │  5  │  6  │     │  7  │  8  │  9  │  0  │  -  │  =  │BACKSPACE  ││HOME │
+   * ├─────┼─────┤├─────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┘  ┌──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬────────┤├─────┤
+   * │ F3  │ F4  ││TAB     │  Q  │  W  │  E  │  R  │  T  │     │  Y  │  U  │  I  │  O  │  P  │  [  │  ]  │\       ││END  │
    * ├─────┼─────┤├────────┴┬────┴┬────┴┬────┴┬────┴┬────┴┐    └┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴────────┤├─────┤
    * │ F5  │ F6  ││CAPS     │  A  │  S  │  D  │  F  │  G  │     │  H  │  J  │  K  │  L  │  ;  │  '  │ENTER        ││PGUP │
    * ├─────┼─────┤├─────────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┐  └──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴───────┬─────┤├─────┤
    * │ F7  │ F8  ││LSHFT       │  Z  │  X  │  C  │  V  │  B  │     │  N  │  M  │  ,  │  .  │  /  │RSHFT     │ UP  ││PGDWN│
    * ├─────┼─────┤├──────┬─────┴─┬───┴──┬──┴───┬──┴────┴─────┤     ├─────┴─────┴────┬┴────┬┴────┬┴────┬─────┼─────┤├─────┤
-   * │ F9  │F10  ││LCTRL │L_ALT  │L_GUI │FN    │SPC          │     │SPC             │R_ALT│R_CTR│APP  │LEFT │DOWN ││RIGHT│
+   * │ F9  │F10  ││LCTRL │L_ALT  │L_GUI │FN    │SPC          │     │ENTER           │DEL  │APP  │R_CTR│LEFT │DOWN ││RIGHT│
    * └─────┴─────┘└──────┴───────┴──────┴──────┴─────────────┘     └────────────────┴─────┴─────┴─────┴─────┴─────┘└─────┘
    */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_0_QWERTY] = LAYOUT_65_with_macro(
-    KC_F1,   KC_F2,   KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  XXXXXXX, KC_BSPC, KC_HOME, \
+    KC_F1,   KC_ESC,  KC_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  XXXXXXX, KC_BSPC, KC_HOME, \
     KC_F3,   KC_F4,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS, KC_END, \
     KC_F5,   KC_F6,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_PGUP, \
     KC_F7,   KC_F8,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   KC_UP,   KC_PGDN, \
-    KC_F9,   KC_F10,  KC_LCTL, KC_LALT, KC_LGUI, MO(_6_FN),KC_SPC, KC_SPC,  XXXXXXX, KC_RALT, KC_APP,  KC_RCTL,                            KC_LEFT, KC_DOWN, KC_RGHT
+    KC_F9,   KC_F10,  KC_LCTL, KC_LALT, KC_LGUI, MO(_6_FN),KC_SPC, KC_ENT,  XXXXXXX, KC_DEL,  KC_APP,  KC_RCTL,                            KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_6_FN] = LAYOUT_65_with_macro(
