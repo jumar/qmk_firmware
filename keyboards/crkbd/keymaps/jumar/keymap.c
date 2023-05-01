a57780c38016ccf984173165c429003def5978c7
Julien Marbach <julien.marbach@dental-wings.com>
2023-04-30 23:28:15 -0400

    Wip OLED SUGAR
    Does not work in combination with RGB_LIGHT / RGB_MATRIX

--- keyboards/crkbd/keymaps/jumar/keymap.c
+++ keyboards/crkbd/keymaps/jumar/keymap.c
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 #include "jumar.h"
+#include "transactions.h"
 
 #ifdef VIRTSER_ENABLE
 #include "virtser.h"
@@ -69,8 +70,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 #ifdef OLED_ENABLE
+//#define OLED_SUGAR
+
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (is_keyboard_master()) {
+    bool rotate_both = false;
+    #ifdef OLED_SUGAR
+    rotate_both = true;
+    #endif
+    if (is_keyboard_master() || rotate_both) {
         return OLED_ROTATION_270;
     } else {
         return rotation;
@@ -85,7 +92,6 @@ void render_crkbd_logo(void) {
         0};
     oled_write_P(crkbd_logo, false);
 }
-
 
 #define KEYLOG_LEN 5
 char     keylog_str[KEYLOG_LEN] = {};
@@ -215,12 +221,199 @@ void render_status_main(void) {
     render_keylogger_status();
 }
 
+//
+// ------------------------ OLED sugar -------------------------
+//
+
+uint16_t keyCntr = 0;
+#define OLED_SUGAR_HEIGHT 128
+#define OLED_SUGAR_WIDTH  32
+bool pixelInvert = false;
+
+void setPixel(char * pixels, uint8_t h, uint8_t w, bool pix) {
+//    oled_write_pixel(w, h, pix);
+    uint16_t byteIdx = w + (h/8)*OLED_SUGAR_WIDTH;
+    int8_t byteMask = 1<<(h%8);
+    if(pixelInvert) pix = !pix;
+    if(pix) {
+        pixels[byteIdx] |= byteMask;
+    } else {
+        pixels[byteIdx] &= ~byteMask;
+    }
+}
+
+bool getPixel(char * pixels, uint8_t h, uint8_t w) {
+    uint16_t byteIdx = w + (h/8)*OLED_SUGAR_WIDTH;
+    int8_t byteMask = 1<<(h%8);
+    bool pix =  (pixels[byteIdx] & byteMask) != 0;
+    if(pixelInvert) pix = !pix;
+    return pix;
+}
+
+uint32_t rand_basic(void) {
+    static uint32_t seed = 0;
+    seed = 1664525*seed + 1013904223;
+    return seed;
+}
+
+#define OLED_SUGAR_WITH_DOWN 1
+
+#define OLED_SUGAR_PIXELS (OLED_SUGAR_HEIGHT*OLED_SUGAR_WIDTH)
+#define OLED_SUGAR_BYTES (OLED_SUGAR_PIXELS/8)
+typedef signed char lineIdx_t;
+lineIdx_t* activeSugar = NULL;
+
+char* pixels = NULL;
+
+void oled_sugar(void) {
+    //return;
+
+    if (activeSugar == NULL) {
+        activeSugar = malloc(OLED_SUGAR_HEIGHT);
+        if (activeSugar != NULL) {
+            memset( activeSugar, -1, OLED_SUGAR_HEIGHT );
+        } else {
+#ifdef PRINT_DB
+            uprintf("activeSugar %X failed to inialise\n", activeSugar);
+#endif
+            return;
+        }
+    }
+
+    if (pixels == NULL) {
+        pixels = malloc(OLED_SUGAR_BYTES);
+        if (pixels != NULL) {
+            memset( pixels, 0, OLED_SUGAR_BYTES );
+        } else {
+#ifdef PRINT_DB
+            uprintf("pixels %X failed to inialise\n", pixels);
+#endif
+            return;
+        }
+
+    }
+
+    static uint8_t sugarCntr = 0;
+
+   for(int16_t h = OLED_SUGAR_HEIGHT-2; h >= 0; h--) {
+        lineIdx_t w = activeSugar[h];
+
+        if(w < 0 || w >= OLED_SUGAR_WIDTH) {
+            continue;
+        }
+        lineIdx_t wn = -1;
+        bool left;
+        bool right;
+#ifdef OLED_SUGAR_WITH_DOWN
+        uint32_t r = rand_basic();
+        const uint32_t thresh1 = UINT32_MAX/3;
+        const uint32_t thresh2 = UINT32_MAX-thresh1;
+        left  = r < thresh1;
+        right = r > thresh2;
+#else
+        left = ((int32_t) rand_basic())<0;
+        right = !left;
+#endif
+        if ( left ) {
+            if( w > 0 ) {
+                if(!getPixel(pixels, h+1, w-1)) {
+                    wn = w - 1;
+                }
+            }
+        } else if (right) {
+            if ( w < OLED_SUGAR_WIDTH-1) {
+                if(!getPixel(pixels, h+1, w+1)) {
+                    wn = w + 1;
+                }
+            }
+        } else { //centre
+            if(!getPixel(pixels, h+1, w)) {
+                wn = w ;
+            }
+        }
+        if(wn == -1) {
+            if(!getPixel(pixels, h+1, w)) {
+                wn = w;
+            }
+        }
+        if(wn != -1) {
+            setPixel(pixels, h+1, wn, true);
+            activeSugar[h+1] = wn;
+            setPixel(pixels, h, w, false);
+        }
+
+        activeSugar[h] = -1;
+    }
+    if(sugarCntr != (keyCntr&0xFF)) {
+
+        sugarCntr++;
+        lineIdx_t w = OLED_SUGAR_WIDTH/2;
+        bool left = false;
+        if( ((int32_t) rand_basic())<0 ) {
+            w--;
+            left = true;
+        }
+        bool full = false;
+        while(getPixel(pixels,0, w)) {
+            if(left) {
+                if (w == 0) {
+                    w = OLED_SUGAR_WIDTH-1;
+                } else if ( w == OLED_SUGAR_WIDTH/2) {
+                    full = true;
+                    break;
+                } else {
+                    w--;
+                }
+            } else {
+                if (w == OLED_SUGAR_WIDTH-1) {
+                    w = 0;
+                } else if ( w == OLED_SUGAR_WIDTH/2-1) {
+                    full = true;
+                    break;
+                } else {
+                    w++;
+                }
+            }
+        }
+
+        if(!full) {
+//        if(!getPixel(pixels, 0, w)) {
+            setPixel(pixels, 0, w, true);
+            activeSugar[0] = w;
+        } else {
+            pixelInvert = !pixelInvert;
+            // we where not able to add a pixel (we where full so add one now that we have inverted)
+            sugarCntr--;
+            oled_sugar();
+        }
+    }
+    rand_basic(); //just here to rotate the seed
+    if(!is_oled_on()) {
+        //OLED timedout so we will clear everything and start fresh
+        memset( pixels, 0, OLED_SUGAR_BYTES );
+        memset( activeSugar, -1, OLED_SUGAR_HEIGHT );
+        oled_clear();
+        pixelInvert = false;
+    } else {
+        oled_write_raw(pixels, OLED_SUGAR_BYTES);
+    }
+
+}
+
+//
+// ------------------------- OLED Sugar End -------------------------
+//
 bool oled_task_user(void) {
     update_log();
     if (is_keyboard_master()) {
         render_status_main();  // Renders the current keyboard state (layer, lock, caps, scroll, etc)
     } else {
+        #ifdef OLED_SUGAR
+        oled_sugar();
+        #else
         render_crkbd_logo();
+        #endif
+
     }
     return false;
 }
@@ -228,7 +421,23 @@ bool oled_task_user(void) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         add_keylog(keycode);
+        #ifdef OLED_ENABLE
+        keyCntr++;
+        transaction_rpc_send(USER_SYNC_KEY_CNTR, sizeof(keyCntr), &keyCntr);
+        #endif
     }
     return true;
 }
-#endif
+
+
+void user_sync_a_update_keyCntr_on_other_board(uint8_t in_buflen, const void* in_data, uint8_t out_buflen, void* out_data) {
+    keyCntr = *((const uint16_t *)in_data);
+}
+
+void keyboard_post_init_user_jumar(void) {
+    #ifdef OLED_ENABLE
+    transaction_register_rpc(USER_SYNC_KEY_CNTR, user_sync_a_update_keyCntr_on_other_board);
+    #endif
+}
+
+#endif // OLED_ENABLE
--- keyboards/idobo/keymaps/jumar/keymap.c
+++ keyboards/idobo/keymaps/jumar/keymap.c
@@ -56,3 +56,100 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   process_record_user_RGB(keycode, record);
   return true;
 }
+
+#ifdef RGBLIGHT_ENABLE
+// RGB LAYER INDICATOR
+void keyboard_post_init_user(void) {
+  // Call the keymap level matrix init.
+
+  // Read the user config from EEPROM
+  user_config.raw = eeconfig_read_user();
+
+  // Set default layer, if enabled
+  if (user_config.rgb_layer_change) {
+    rgblight_enable_noeeprom();
+    rgblight_sethsv_noeeprom_blue();
+    rgblight_mode_noeeprom(1);
+  }
+}
+
+uint32_t layer_state_set_user(uint32_t state) {
+    uint8_t layer = get_highest_layer(state);
+    switch (layer) {
+        case _0_QWERTY:
+            if (user_config.rgb_layer_change) { rgblight_sethsv_noeeprom_blue(); rgblight_mode_noeeprom(1); }
+            break;
+        case _1_SYMBOLS_NUMPAD:
+            if (user_config.rgb_layer_change) { rgblight_sethsv_noeeprom_chartreuse(); rgblight_mode_noeeprom(1); }
+            break;
+        case _2_MOUSE_MEDIA:
+            if (user_config.rgb_layer_change) { rgblight_sethsv_noeeprom_coral(); rgblight_mode_noeeprom(1); }
+            break;
+        case _3_NAV:
+            if (user_config.rgb_layer_change) { rgblight_sethsv_noeeprom_red(); rgblight_mode_noeeprom(1); }
+            break;
+        case _4_GAM:
+            if (user_config.rgb_layer_change) { rgblight_sethsv_noeeprom_orange(); rgblight_mode_noeeprom(1); }
+            break;
+        case _5_RGB:
+            if (user_config.rgb_layer_change) { rgblight_sethsv_noeeprom_teal(); rgblight_mode_noeeprom(1); }
+            break;
+        case _6_FN:
+            if (user_config.rgb_layer_change) { rgblight_sethsv_noeeprom_purple(); rgblight_mode_noeeprom(1); }
+            break;
+        default: //  for any other layers, or the default layer
+            if (user_config.rgb_layer_change) { rgblight_sethsv_noeeprom_white(); rgblight_mode_noeeprom(1); }
+            break;
+    }
+  return state;
+}
+
+bool process_record_user_RGB(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case RGB_LYR:  // This allows me to use underglow as layer indication, or as normal
+        if (record->event.pressed) {
+            user_config.rgb_layer_change ^= 1; // Toggles the status
+            eeconfig_update_user(user_config.raw); // Writes the new status to EEPROM
+            if (user_config.rgb_layer_change) { // if layer state indication is enabled,
+                layer_state_set(layer_state);   // then immediately update the layer color
+            }
+        }
+        return false; break;
+    case RGB_MODE_FORWARD ... RGB_MODE_GRADIENT: // For any of the RGB codes (see quantum_keycodes.h, L400 for reference)
+        if (record->event.pressed) { //This disables layer indication, as it's assumed that if you're changing this ... you want that disabled
+            if (user_config.rgb_layer_change) {        // only if this is enabled
+                user_config.rgb_layer_change = false;  // disable it, and
+                eeconfig_update_user(user_config.raw); // write the setings to EEPROM
+            }
+        }
+        return true; break;
+    default:
+      return true; // Process all other keycodes normally
+  }
+}
+
+void eeconfig_init_user(void) {  // EEPROM is getting reset!
+  user_config.raw = 1;
+  user_config.rgb_layer_change = true; // We want this enabled by default
+  eeconfig_update_user(user_config.raw); // Write default value to EEPROM now
+
+  // use the non noeeprom versions, to write these values to EEPROM too
+  rgblight_enable(); // Enable RGB by default
+  rgblight_sethsv_noeeprom_blue();  // Set it to oranrge by default
+  rgblight_mode(1); // set to solid by default
+}
+
+void matrix_init_user_RGB_LYR(void) {
+  // Call the keymap level matrix init.
+
+  // Read the user config from EEPROM
+  user_config.raw = eeconfig_read_user();
+
+  // Set default layer, if enabled
+  if (user_config.rgb_layer_change) {
+    rgblight_enable_noeeprom();
+    rgblight_sethsv_noeeprom_blue();
+    rgblight_mode_noeeprom(1);
+  }
+}
+#endif
